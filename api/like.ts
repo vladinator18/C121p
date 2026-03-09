516971379c3201d965975f46184d9a2e57a804f7
Or10n <93318326+vladinator18@users.noreply.github.com>
2026-03-09 19:02:01 +0800

    updated - installed prisma and depts

--- api/like.ts
+++ api/like.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { PrismaClient } from '../generated/prisma.js';
+import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
 
